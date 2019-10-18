9e30c78f06d176ad904afd27340da76fd689e8fb
dongwon <lemontech119@gmail.com>
2019-10-18 13:59:52 +0900

    아무도 모르는 빨간모자피자 크롤링

---
+++ crawling/pizza_redcap.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+chromedriver = 'C:\coding\chromedriver.exe'
+
+driver = webdriver.Chrome(chromedriver)
+
+menu = 'http://www.redcappizza.com/menu/pizza.jsp'
+
+driver.get(menu)
+
+time.sleep(1)
+
+menuArray = []
+
+menuList = driver.find_element_by_class_name('gdslist').find_element_by_tag_name('ul')
+menuList = menuList.find_elements_by_tag_name('li')
+
+for menu in menuList:
+    idx = menu.get_attribute('idx')
+    if idx:
+        menuArray.append(idx)
+
+menuView = 'http://www.redcappizza.com/menu/menu_view.jsp?pdid='
+
+print(menuArray)
+
+driver.quit()
